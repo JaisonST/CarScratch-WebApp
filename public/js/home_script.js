5c17fd16af4d41df16a05e36977a8701ed7ed1d9
JaisonST <jaisonmanu@gmail.com>
2022-06-03 11:54:30 +0400

    reload page on submit

--- public/js/home_script.js
+++ public/js/home_script.js
@@ -14,6 +14,13 @@ window.onclick = function(event) {
     }
 }
 
+window.addEventListener( "pageshow", function ( event ) {
+    var historyTraversal = event.persisted || ( typeof window.performance != "undefined" && window.performance.navigation.type === 2 );
+    if ( historyTraversal ) {
+      window.location.reload();
+    }
+});
+
 function formSubmit(){
     closeModal();
 
